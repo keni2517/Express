6da30dc807c84250a4f4ca68529a43b9e5915091
keni <kenikakadiya@gmail.com>
2024-08-06 09:51:29 +0530

    6/8/24

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const server1 = express();
+
+server1.get('/',(req,res)=>{
+    res.send('welcome to Express');
+    res.end();
+})
+
+server1.get('/login',(req,res)=>{
+    res.send({msg:"welcome to Login Page"});
+    res.end();
+})
+
+server1.listen(2609,()=>{
+    console.log('server start at http://localhost:2609'); 
+})
